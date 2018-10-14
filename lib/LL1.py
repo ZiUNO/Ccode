d5d01e606f6c4a9b5c340a7dd81fb5b473b4fdf1
ZiUNO <331245059@qq.com>
2018-10-14 22:03:23 +0800

    Add: LL(1) to check statement

--- lib/LL1.py
+++ lib/LL1.py
@@ -9,7 +9,8 @@
 
 class LL1:
     STATE = {
-        "SYNCH": 1
+        "SYNCH": 1,
+        "ERROR": 2
     }
 
     def __init__(self, ffset):
@@ -62,3 +63,41 @@ class LL1:
                 elif tmpTermi in self.__followSet[e]:
                     self.__chart[e][tmpTermi] = "SYNCH"
         return
+
+    def check(self, toCheck):
+        process = []
+        message = []
+        count = 0
+        position = None
+        toCheck = list(reversed(toCheck))
+        if toCheck[0] != '$':
+            toCheck.insert(0, '$')
+        stack = ['$', self.__ffs.getFirstNontermi()]
+        tmpSymbol = stack.pop()
+        tmpToCheck = toCheck.pop()
+        columnCount = 0
+        separator = '\n'
+        while len(stack) != 0:
+            # 匹配成功
+            if tmpToCheck == tmpSymbol:
+                tmpSymbol = stack.pop()
+                tmpToCheck = toCheck.pop()
+                columnCount += 1
+                continue
+            expression = self.__chart[tmpSymbol][tmpToCheck]
+            if expression == "SYNCH" or expression == "ε":
+                tmpSymbol = stack.pop()
+                if expression == "SYNCH":
+                    count += 1
+                    message.append('[ERROR(No.%d)]missing %s at %d column' % (count, tmpToCheck, columnCount))
+            elif expression == "ERROR":
+                count += 1
+                columnCount += 1
+                message.append('[ERROR(No.%d)]unexpected %s at %d column' % (count, tmpToCheck, columnCount))
+                tmpToCheck = toCheck.pop()
+            else:
+                process.append('%s->%s' % (tmpSymbol, expression))
+                stack += list(reversed(expression))
+                tmpSymbol = stack.pop()
+        message.append('total %d error(s)' % count)
+        return separator.join(process), separator.join(message)
